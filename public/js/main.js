6a477c46cff98aebc38e92cba7246b8d1017cb17
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-24 13:33:50 -0300

    inicial

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+angular.module('portal', ['ngRoute', 'ngResource'])
+    .config(function($routeProvider, $httpProvider){
+
+        $routeProvider.when('/', {
+            templateUrl: 'index.html'
+        });
+
+      $routeProvider.otherwise({redirectTo: '/'});
+
+    });
