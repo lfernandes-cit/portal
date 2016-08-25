c24f81ba178fb440e67db10279ccce86ad5bff3f
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-25 14:50:38 -0300

    cookie

--- public/js/controllers/EcController.js
+++ public/js/controllers/EcController.js
@@ -1,8 +1,8 @@
-angular.module('portal').controller('EcController' , function ($scope, $http) {
+angular.module('portal').controller('EcController' , function ($scope, $http, $cookies) {
 	
-	$scope.teste = 'testefhjd';
-	
-	$http.get('http://localhost:8080/portal/ec?idUsuario=2').then(function (response) {
+    $scope.teste = 'testefhjd';
+
+	$http.get('http://172.16.18.120:8080/portal/ec?idUsuario=2').then(function (response) {
 		console.log(response.data.object);
 		$scope.ecs = response.data.object;
 		
