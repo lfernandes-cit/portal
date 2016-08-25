f79d27fbb20f72f0fe3faf42e736ba66e8d8a7d6
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-25 10:46:39 -0300

    descricao log quando err

--- public/js/controller/loginController.js
+++ public/js/controller/loginController.js
@@ -2,12 +2,13 @@ angular.module('portal').controller("LoginController", function($scope, $http){
 
     $scope.logar = function(usuario){
         console.log(usuario);
-        $http.post("http://172.16.15.204:8080/portal/usuario", usuario).then(
+
+        $http.post("http://172.16.18.120:8080/portal/usuario?login=usuario&senha=usuario").then(
         function(retorno){
             console.log(retorno);
         },
         function(erro){
-            console.log(erro);        
+            console.log("Erro"+ erro); 
         }); 
     }
 
