b7d4509bf6ca478f6b30fbecab83ece895316625
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-26 10:00:07 -0300

    mensagem de erro

--- public/js/controllers/loginController.js
+++ public/js/controllers/loginController.js
@@ -1,14 +1,18 @@
 angular.module('portal').controller("LoginController", function($scope, $rootScope, $http, $location, $cookies, IpFactory){
 
+    $scope.message = "";
+
     $scope.logar = function(usuario){
         console.log(usuario);
 
         $http.post(IpFactory.host + "/portal/usuario", usuario).then(
         function(retorno){
-            if(retorno.data && retorno.data.object.id){
+            if(retorno.data && retorno.data.object){
                 $cookies.putObject("usuarioLogado", retorno.data.object);
                 $rootScope.usuarioLogado = $cookies.getObject("usuarioLogado");
                 $location.path( "/ec");
+            }else{
+              $scope.message = "Usuario ou senha inválido.";
             }
         },
         function(erro){
