353929305f10b0e1b28b7c33855f51a571d0b242
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-26 15:53:42 -0300

    excluirTodos

--- public/js/controllers/EcController.js
+++ public/js/controllers/EcController.js
@@ -47,13 +47,15 @@ angular.module('portal').controller('EcController' , function ($scope, $http, $c
 
 	}
 
-	$scope.excluirAll = function(){
-		$scope.ecs.map(function(ec){
-			if (ec.selecionado) {
-				$scope.excluir(ec.id);
-			}
-		});
-		listaEc();
+	$scope.excluirTodos = function(){
+		if ($scope.ecs && $scope.ecs.length > 0) {
+			$scope.ecs.map(function(ec){
+				if (ec.selecionado) {
+					$scope.excluir(ec.id);
+				}
+			});
+			listaEc();
+		}
 	}
 
 	listaEc();
