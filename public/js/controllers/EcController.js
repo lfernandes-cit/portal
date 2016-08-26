c96d03e79e4aa44f359b2ad65519f735b5d23876
Fernando da Silva Rosa <fernandosvrosa@gmail.com>
2016-08-26 15:29:29 -0300

    metodo delete

--- public/js/controllers/EcController.js
+++ public/js/controllers/EcController.js
@@ -1,12 +1,15 @@
 angular.module('portal').controller('EcController' , function ($scope, $http, $cookies, $location, IpFactory) {
-	
+
 	var idUsuario = $cookies.getObject("usuarioLogado").id;
-	
-	$http.get(IpFactory.host + '/portal/ec/usuario/?idUsuario='+idUsuario).then(function (response) {
-		$scope.ecs = response.data.object;
-		
-	});
-	
+
+
+	listaEc = function(){
+		$http.get(IpFactory.host + '/portal/ec/usuario/?idUsuario='+idUsuario).then(function (response) {
+			$scope.ecs = response.data.object;
+
+		});
+	}
+
 	$scope.selecionarTodos = function () {
 
 		if ($scope.checkSelecionarTodos) {
@@ -14,23 +17,36 @@ angular.module('portal').controller('EcController' , function ($scope, $http, $c
 		} else {
 			$scope.checkSelecionarTodos = false;
 		}
-		
+
 		angular.forEach($scope.ecs, function (ec){
 			ec.selecionado = $scope.checkSelecionarTodos;
-		}); 
+		});
 	}
-	
+
 	$scope.ordenarPor = function (campo) {
 		$scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
 		$scope.criterioDeOrdenacao = campo;
 	}
-	
+
 	$scope.adicionarEc = function () {
 		$location.path( "/ec/adicionar");
 	}
 
 	$scope.editarEc = function (ec) {
-		$location.path( "/ec/editar/" + ec.id);	
+		$location.path( "/ec/editar/" + ec.id);
 	}
-	
+
+	$scope.excluir = function(id){
+		$http.delete(IpFactory.host + '/portal/ec?id='+id).then(
+		function(retorno){
+			listaEc();
+		},
+		function(erro){
+				console.log("Erro"+ erro);
+		});
+
+	}
+
+	listaEc();
+
 });
